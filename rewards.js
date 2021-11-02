482e84cbb522b261f0922710eb8e5b076b9a1ac6
vrerabek <vrerabek@gmail.com>
2021-11-02 02:23:06 +0100

    9.1.5 update

---
+++ rewards.js
@@ -0,0 +1,8 @@
+export {rewards}
+/**
+ * Additional sources of Soul Cinders (or Soul Ash)
+ */
+let rewards = {
+    'tormentorsCinders' : 100,
+    'assaultCinders'    : 100,
+}
---
+++ soulRewards.js
@@ -0,0 +1,55 @@
+export {soulRewards}
+
+/**
+ * Soul Ash and Soul Cinders rewards for Torghast layers
+ */
+let soulRewards = {
+    '1' : {
+        'soulAsh': 180,
+        'soulCinders': 0,
+    },
+    '2': {
+        'soulAsh': 330,
+        'soulCinders': 0,
+    },
+    '3' : {
+        'soulAsh': 460,
+        'soulCinders': 0,
+    },
+    '4': {
+        'soulAsh': 565,
+        'soulCinders': 0,
+    },
+    '5': {
+        'soulAsh': 655,
+        'soulCinders': 0,
+    },
+    '6': {
+        'soulAsh': 730,
+        'soulCinders': 0,
+    },
+    '7': {
+        'soulAsh': 800,
+        'soulCinders': 0,
+    },
+    '8': {
+        'soulAsh': 860,
+        'soulCinders': 170,
+    },
+    '9': {
+        'soulAsh': 915,
+        'soulCinders': 230,
+    },
+    '10': {
+        'soulAsh': 965,
+        'soulCinders': 270,
+    },
+    '11': {
+        'soulAsh': 1000,
+        'soulCinders': 310,
+    },
+    '12': {
+        'soulAsh': 1030,
+        'soulCinders': 350,
+    }
+}
