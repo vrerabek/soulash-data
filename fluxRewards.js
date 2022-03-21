0338ab912eae1adeb45898e24c12dbd9f2188948
vrerabek <vrerabek@gmail.com>
2022-03-21 23:18:30 +0100

    Update fluxRewards.js
    changed m+ reward

--- fluxRewards.js
+++ fluxRewards.js
@@ -3,12 +3,12 @@ export {fluxRewards}
  * Additional sources of Cosmic Flux
  */
 let fluxRewards = {
-    'mythicPlusFlux' : 100,
+    'mythicPlusFlux' : '87-100',
     'arenaWin': 100,
     'raidBoss': 100,
     'greatVault': 500,
 
-    
+
     // Zereth Mortis
     'rares': '50-75',
     'dailies': 75,
